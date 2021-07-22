df58233b2839a474d90411872b6c102753dbe9de
kshannn <k.leshan@hotmail.com>
2021-07-22 09:19:31 +0000

    Added function to remove cart item

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,14 +1,16 @@
 import axios from 'axios'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import config from '../config'
-
-//tmp
-// import { useLocation } from 'react-router-dom'
+import UserContext from './UserContext'
 
 export default function Cart() {
 
     // decodedAccessToken
-    let decoded = JSON.parse(localStorage.getItem('decodedAccessToken'))
+    // let decoded = JSON.parse(localStorage.getItem('decodedAccessToken'))
+
+    let context = useContext(UserContext);
+
+
     
    const [ cartItems, setCartItems ] = useState([])
 
@@ -16,24 +18,34 @@ export default function Cart() {
     fetchCartItems()
   }, [])
 
+    // remove cart item
+    let removeCartItem = async (gameId) => {
+        // console.log(context.userInfo().id)
+        await axios.post(config.API_URL + '/cart/' + gameId + '/remove', {
+            'user_id': context.userInfo().id
+        })
+        // re-render page to reflect updated changes
+        fetchCartItems();
+    }
+
     // fetch cart items based on user id
     let fetchCartItems = async () => {
-        let response = await axios.get(config.API_URL + '/cart/' + decoded.id)        
+        let response = await axios.get(config.API_URL + '/cart/' + context.userInfo().id)        
         setCartItems(response.data)
-        console.log(response.data)
     }
 
     let renderCartItems = () => {
         let cartItemsjsx = cartItems.map((item)=> {
             return (
-                <p>id: {item.gameListing.id} {item.gameListing.name} x {item.quantity}</p>
+                <p>id: {item.gameListing.id} {item.gameListing.name} x {item.quantity} <button onClick={() => {
+                    removeCartItem(item.gameListing.id)
+                }}>Remove from cart</button></p>
             )
         })
         return cartItemsjsx
     }
 
-    return (
-        
+    return (    
         <React.Fragment>
             <h1>User's Cart</h1> 
             {renderCartItems()}
