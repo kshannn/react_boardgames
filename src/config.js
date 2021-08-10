2a2b968f27e844d42261327db8f17f0a4feabc35
kshannn <k.leshan@hotmail.com>
2021-08-10 07:32:34 +0000

    Updated spelling

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     'API_URL': "https://kls-boardgames.herokuapp.com/api",
-    'REACT_URL': "https://loving-mayer-9f2c72.netlify.app/"
+    'REACT_URL': "https://loving-mayer-9f2c72.netlify.app"
     // 'REFRESH_INTERVAL': 840000
 }
 
